69c2bf1413ce1b2ac09622f7fe310e380f605630
tuliparc <tulipa.rc@gmail.com>
2020-12-11 18:47:53 +0000

    Gerando entradas.html

--- flaskr.py
+++ flaskr.py
@@ -17,3 +17,18 @@ app = Flask(__name__)
 
 def conectar_bd():
     return sqlite3.connect(DATABASE)
+
+@app.before_request
+def pre_request():
+    g.bd = conectar_bd
+
+@app.teardown_request
+def pos_request():
+    g.bd.close()
+
+@app.route('/')
+def exibir_entradas():
+    sql= "SELECT titulo, texto FROM entradas ORDER BY id DESC"
+    cursor = g.bd.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto) for titulo,texto in cursor.fetchall()]
+    return render_template('exibir_entradas.html')
